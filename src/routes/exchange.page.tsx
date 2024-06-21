9121be282ee607a936c4d3dcd49b09f38518af02
Bruno Fukumori <brunofukumori@gmail.com>
2024-06-21 20:37:40 -0300

    feat: add interactivity

--- src/routes/exchange.page.tsx
+++ src/routes/exchange.page.tsx
@@ -4,15 +4,17 @@ import {
   SelectItem,
   SelectTrigger,
 } from "@radix-ui/react-select";
+import { ArrowRightLeft } from "lucide-react";
+import { useState } from "react";
 
 import bitcoin from "@/assets/bitcoin.svg";
 import ethereum from "@/assets/ethereum.svg";
 import solana from "@/assets/solana.svg";
 import tether from "@/assets/tether.svg";
-import { SelectValue } from "@/components/ui/Select";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Button } from "@/components/ui/Button";
-import { ArrowRightLeft } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
+import { SelectValue } from "@/components/ui/Select";
+import { currencyFormatter, percentFormatter } from "@/lib/utils";
 
 const CRYPTO_NAME: Record<string, string> = {
   BTC: "Bitcoin",
@@ -29,57 +31,86 @@ const CRYPTO_IMG: Record<string, string> = {
 };
 
 export function ExchangePage() {
+  const [cryptoOrigin, setCryptoOrigin] = useState("SOL");
+  const [cryptoFinal, setCryptoFinal] = useState("ETH");
+
   return (
     <div className="flex flex-col gap-6 px-4">
       <div className="flex gap-6">
         <Card className="flex-1">
           <CardHeader>
             <CardTitle className="mb-4 text-lg">TROCAR</CardTitle>
-
-            <Select>
-              <SelectTrigger id="framework">
-                <SelectValue
-                  placeholder={
-                    <div className="flex items-center">
-                      <img src={CRYPTO_IMG.SOL} width={20} height={20}></img>
-                      <span className="ml-4">{CRYPTO_NAME.SOL}</span>
-                    </div>
-                  }
-                  className="font-bold"
-                />
+            <Select value={cryptoOrigin} onValueChange={setCryptoOrigin}>
+              <SelectTrigger>
+                <SelectValue>
+                  <div className="flex items-center">
+                    <img
+                      src={CRYPTO_IMG[cryptoOrigin]}
+                      width={20}
+                      height={20}
+                    ></img>
+                    <span className="ml-4">{CRYPTO_NAME[cryptoOrigin]}</span>
+                  </div>
+                </SelectValue>
               </SelectTrigger>
-              <SelectContent position="popper">
-                <SelectItem value="next">Cardano</SelectItem>
+              <SelectContent
+                className="w-[250px] rounded bg-slate-800 shadow-2xl"
+                position="popper"
+              >
+                <SelectItem className="cursor-pointer p-4" value="BTC">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.BTC} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.BTC}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="ETH">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.ETH} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.ETH}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="SOL">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.SOL} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.SOL}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="USDT">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.USDT} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.USDT}</span>
+                  </div>
+                </SelectItem>
               </SelectContent>
             </Select>
             <div className="flex space-x-20 pl-16">
               <div className="mt-8">
-                <CardTitle className="text-sm">ULTIMO PREÇO</CardTitle>
-                <CardTitle className="text-center text-sm text-red-500">
-                  R$ 2,2272
-                </CardTitle>
+                <p className="text-sm font-semibold">ULTIMO PREÇO</p>
+                <p className="text-center text-sm text-secondary-foreground">
+                  {currencyFormatter(2.2272)}
+                </p>
               </div>
               <div className="mt-8">
-                <CardTitle className="text-sm">VARIAÇÃO 24H</CardTitle>
-                <CardTitle className="text-center text-sm text-red-500">
-                  1,60%
-                </CardTitle>
+                <p className="text-sm font-semibold">VARIAÇÃO 24H</p>
+                <p className="text-center text-sm text-emerald-500">
+                  +{percentFormatter(0.016)}
+                </p>
               </div>
             </div>
           </CardHeader>
           <CardContent>
-            <CardTitle className="flex justify-end">2.227,7416 ADA</CardTitle>
+            <p className="flex justify-end text-2xl font-semibold">
+              2.227,7416 {cryptoOrigin}
+            </p>
             <div className="flex pb-4 pt-16">
-              <CardTitle className="text-sm">Usar saldo disponível: </CardTitle>
-              <CardTitle className="pl-2 text-sm font-bold">
-                2.227,74161345
-              </CardTitle>
+              <p className="text-sm">Usar saldo disponível: </p>
+              <p className="pl-2 text-sm font-bold">2.227,74161345</p>
             </div>
             <div className="flex space-x-4">
-              <Button>100%</Button>
-              <Button>75%</Button>
-              <Button>50%</Button>
-              <Button>25%</Button>
+              <Button>{percentFormatter(1)}</Button>
+              <Button>{percentFormatter(0.75)}</Button>
+              <Button>{percentFormatter(0.5)}</Button>
+              <Button>{percentFormatter(0.25)}</Button>
             </div>
           </CardContent>
         </Card>
@@ -87,46 +118,71 @@ export function ExchangePage() {
         <Card className="flex-1">
           <CardHeader>
             <CardTitle className="mb-4 text-lg">POR</CardTitle>
-            <Select>
-              <SelectTrigger id="framework">
-                <SelectValue
-                  placeholder={
-                    <div className="flex items-center">
-                      <img src={CRYPTO_IMG.ETH} width={20} height={20}></img>
-                      <span className="ml-4">{CRYPTO_NAME.ETH}</span>
-                    </div>
-                  }
-                  className="font-bold"
-                />
+            <Select value={cryptoFinal} onValueChange={setCryptoFinal}>
+              <SelectTrigger>
+                <SelectValue>
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG[cryptoFinal]} width={20} height={20} />
+                    <span className="ml-4">{CRYPTO_NAME[cryptoFinal]}</span>
+                  </div>
+                </SelectValue>
               </SelectTrigger>
-              <SelectContent className="" position="popper">
-                <SelectItem value="next">Ethereum</SelectItem>
+              <SelectContent
+                className="w-[250px] rounded bg-slate-800 shadow-2xl"
+                position="popper"
+              >
+                <SelectItem className="cursor-pointer p-4" value="BTC">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.BTC} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.BTC}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="ETH">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.ETH} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.ETH}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="SOL">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.SOL} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.SOL}</span>
+                  </div>
+                </SelectItem>
+                <SelectItem className="cursor-pointer p-4" value="USDT">
+                  <div className="flex items-center">
+                    <img src={CRYPTO_IMG.USDT} width={16} height={16} />
+                    <span className="ml-4">{CRYPTO_NAME.USDT}</span>
+                  </div>
+                </SelectItem>
               </SelectContent>
             </Select>
             <div className="flex space-x-20 pl-16">
               <div className="mt-8">
-                <CardTitle className="text-sm">ULTIMO PREÇO</CardTitle>
-                <CardTitle className="text-center text-sm text-red-500">
-                  R$ 19.170,23
-                </CardTitle>
+                <p className="text-sm font-semibold">ULTIMO PREÇO</p>
+                <p className="text-center text-sm text-secondary-foreground">
+                  {currencyFormatter(19170.23)}
+                </p>
               </div>
               <div className="mt-8">
-                <CardTitle className="text-sm">VARIAÇÃO 24H</CardTitle>
-                <CardTitle className="text-center text-sm text-red-500">
-                  1,82%
-                </CardTitle>
+                <p className="text-sm font-semibold">VARIAÇÃO 24H</p>
+                <p className="text-center text-sm text-red-500">
+                  -{percentFormatter(0.0182)}
+                </p>
               </div>
             </div>
           </CardHeader>
           <CardContent>
-            <CardTitle className="flex justify-end">0,2554 ETH</CardTitle>
+            <p className="flex justify-end text-2xl font-semibold">
+              0,2554 {cryptoFinal}
+            </p>
             <div className="flex justify-end pb-2 pt-20">
-              <CardTitle className="text-sm">
-                Valor aproximado da trasnsação:
-              </CardTitle>
+              <p className="text-sm">Valor aproximado da transação:</p>
             </div>
             <div className="flex justify-end pt-2">
-              <CardTitle className="text-sm font-bold">R$ 4.897,69</CardTitle>
+              <p className="text-lg font-semibold text-primary">
+                {currencyFormatter(4897.69)}
+              </p>
             </div>
           </CardContent>
         </Card>
